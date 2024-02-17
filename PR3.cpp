43c1923403a667865d519da0298cf1cdd5ed1280
ttw1St0fF4te <isip_e.e.novikov@mpt.ru>
2024-02-17 09:22:49 +0300

    Добавьте файлы проекта.

---
+++ PR3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float num_array[3][5] = {};
+    float num = 0;
+
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int k = 0; k < 5; k++) 
+        {
+            cin >> num_array[i][k];
+            num += num_array[i][k];
+        }
+    }
+    cout << num / 15 << endl;
+}
